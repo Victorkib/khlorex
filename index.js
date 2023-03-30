5a5c63acb7d11668b83956d2a3e094a03ff08222
Vic <107037865+Victorkib@users.noreply.github.com>
2023-03-30 08:38:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let btn=document.getElementById("button");
+ 
+
+btn.addEventListener('click',function(){
+  let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+   if( username.value=true ){
+    if(password.value=true){
+
+        window.location="index.html";
+        alert("Welcome!!!");
+    }
+   }
+
+});
